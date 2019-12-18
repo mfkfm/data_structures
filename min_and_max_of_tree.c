caeedcb7374c7a18191c5e19524f5bc2374891ab
mfkfm <59029131+mfkfm@users.noreply.github.com>
2019-12-18 22:30:42 +0300

    Create min_and_max_of_tree.c

---
+++ min_and_max_of_tree.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+struct n{
+	int data;
+	struct n * sol;
+	struct n * sag;
+};
+
+typedef struct n node;
+
+node * ekle (node *agac, int x){
+	if(agac==NULL){
+		node *root= (node *)malloc(sizeof(node));
+		root->sol=NULL;
+		root->sag=NULL;
+		root->data=x;
+		return root;
+	}
+	if(agac->data < x){
+		agac->sag=ekle(agac->sag, x);
+		return agac;
+	}
+	agac->sol=ekle(agac->sol, x);
+	return agac;
+}
+
+int max(node *agac){
+	while(agac->sag!=NULL)
+		agac=agac->sag;
+	return agac->data;
+	
+}
+int min(node *agac){
+	while(agac->sol!=NULL)
+		agac=agac->sol;
+	return agac->data;
+	
+}
+int main(){
+	node * agac=NULL;
+	agac=ekle(agac, 12);
+	agac=ekle(agac, 200);
+	agac=ekle(agac, 190);
+	agac=ekle(agac, 213);
+	agac=ekle(agac, 215);
+	agac=ekle(agac, 216);
+	agac=ekle(agac, 56);
+	agac=ekle(agac, 24);
+	agac=ekle(agac, 18);
+	agac=ekle(agac, 27);
+	agac=ekle(agac, 28);
+	agac=ekle(agac, 17);
+	agac=ekle(agac, 19);
+	
+	min(agac);
+	max(agac);
+	printf("maksimum= %d \n", max(agac));
+	printf("minimum= %d \n", min(agac));
+	
+
+}
